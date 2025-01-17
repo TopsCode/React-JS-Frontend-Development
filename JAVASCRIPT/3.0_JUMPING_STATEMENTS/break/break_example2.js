0286e84ed7ba801bf7a0ae589f66466ae6eff5b1
enji-coder <kailash.solanki@tops-int.com>
2025-01-17 14:48:29 +0530

    break

---
+++ JAVASCRIPT/3.0_JUMPING_STATEMENTS/break/break_example2.js
@@ -0,0 +1,15 @@
+let fruit = 'apple';
+
+switch(fruit) {
+    case 'banana':
+        console.log('It is a banana');
+        break;  // Exit the switch after the case is executed
+    case 'apple':
+        console.log('It is an apple');
+        break;  // Exit the switch after the case is executed
+    case 'orange':
+        console.log('It is an orange');
+        break;
+    default:
+        console.log('Unknown fruit');
+}
