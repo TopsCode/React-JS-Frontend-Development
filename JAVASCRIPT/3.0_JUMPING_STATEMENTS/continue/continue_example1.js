8889727da47a1807baf11711179f88ff66817b9d
enji-coder <kailash.solanki@tops-int.com>
2025-01-17 15:04:30 +0530

    updates

---
+++ JAVASCRIPT/3.0_JUMPING_STATEMENTS/continue/continue_example1.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        continue;  // Skip the iteration when i equals 3
+    }
+    console.log(i);
+}
---
+++ JAVASCRIPT/3.0_JUMPING_STATEMENTS/continue/continue_example2.js
@@ -0,0 +1,8 @@
+let i = 0;
+while (i < 5) {
+    i++;
+    if (i === 3) {
+        continue;  // Skip iteration when i equals 3
+    }
+    console.log(i);
+}
