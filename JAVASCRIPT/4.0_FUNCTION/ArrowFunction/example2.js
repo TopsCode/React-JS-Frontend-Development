30f221c6ea1b9c39ce80aecb45500a3e458a56f4
enji-coder <kailash.solanki@tops-int.com>
2025-01-18 09:53:52 +0530

    updates

---
+++ JAVASCRIPT/4.0_FUNCTION/ArrowFunction/example2.js
@@ -0,0 +1,2 @@
+const add = (a, b) => a + b;  // Implicit return
+console.log(add(3, 4));  // Output: 7
---
+++ JAVASCRIPT/4.0_FUNCTION/ArrowFunction/object_example.js
@@ -0,0 +1,2 @@
+const createPerson = (name, marks) => ({ name, marks });
+console.log(createPerson('Anjali', 89)); // { name: 'Anjali', marks: 89 }
