4d328883fb61e7cdc8ede338ebfbb6c91ec2a69c
enji-coder <kailash.solanki@tops-int.com>
2025-01-18 10:21:28 +0530

    global scope

---
+++ JAVASCRIPT/5.0_SCOPE/GLOBAL_SCOPE/global_scope_example.js
@@ -0,0 +1,8 @@
+let globalVariable = "I am global";
+
+function printGlobal() {
+  console.log(globalVariable); // Accessible here
+}
+
+printGlobal(); // "I am global"
+console.log(globalVariable); // "I am global"
