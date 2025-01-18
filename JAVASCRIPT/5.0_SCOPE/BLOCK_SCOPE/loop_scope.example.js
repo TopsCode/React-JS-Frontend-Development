38dc74cc54d40b217cc17c4b6b1e8e5ea840da6c
enji-coder <kailash.solanki@tops-int.com>
2025-01-18 13:22:14 +0530

    loop

--- JAVASCRIPT/5.0_SCOPE/BLOCK_SCOPE/loop_scope.example.js
+++ JAVASCRIPT/5.0_SCOPE/BLOCK_SCOPE/loop_scope.example.js
@@ -3,3 +3,9 @@ for (let i = 0; i < 3; i++) {
 }
 
 console.log(i);  // ReferenceError: i is not defined
+
+for (var i = 0; i < 3; i++) {
+    console.log(i);  // Outputs: 0, 1, 2
+}
+
+console.log(i);  // 3
