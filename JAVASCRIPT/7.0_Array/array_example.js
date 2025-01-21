f9406ac04c2a7e12f87b0341a1d0ff46db624c3d
enji-coder <kailash.solanki@tops-int.com>
2025-01-21 12:37:04 +0530

    array example

---
+++ JAVASCRIPT/7.0_Array/array_example.js
@@ -0,0 +1,5 @@
+let fruits = ["Apple", "Banana", "Cherry"];
+
+console.log(fruits[0]);  // Output: "Apple"
+console.log(fruits[1]);  // Output: "Banana"
+console.log(fruits[2]);  // Output: "Cherry"
