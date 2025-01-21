14d8c0ee5e3dca81cf983eed05149d6a014b3dae
enji-coder <kailash.solanki@tops-int.com>
2025-01-21 13:05:04 +0530

    updates

---
+++ JAVASCRIPT/7.0_Array/array_constructor_2.js
@@ -0,0 +1,9 @@
+let fruits = new Array("Apple", "Banana", "Cherry");
+
+console.log(fruits[0]);  // Output: "Apple"
+console.log(fruits[1]);  // Output: "Banana"
+console.log(fruits[2]);  // Output: "Cherry"
+
+//If you try to access an index that doesn't exist, you get undefined:
+
+console.log(fruits[3]);  // Output: undefined
