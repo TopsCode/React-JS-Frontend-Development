83762c5f712e50f8d7cb13162e47d83253756e39
enji-coder <kailash.solanki@tops-int.com>
2025-01-21 15:01:49 +0530

    array

---
+++ JAVASCRIPT/7.0_Array/array_destructure.js
@@ -0,0 +1,6 @@
+let fruits = ["Apple", "Banana", "Cherry"];
+let [first, second, third] = fruits;
+
+console.log(first);  // Output: "Apple"
+console.log(second); // Output: "Banana"
+console.log(third);  // Output: "Cherry"
---
+++ JAVASCRIPT/7.0_Array/multi_dimesional.js
@@ -0,0 +1,8 @@
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+console.log(matrix[0][0]); // Output: 1
+console.log(matrix[1][2]); // Output: 6
+  
