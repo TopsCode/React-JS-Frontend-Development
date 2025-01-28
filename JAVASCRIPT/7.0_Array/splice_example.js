bc92fec6cedf93d2e8896ad9a5f1ca6d66de90e4
enji-coder <kailash.solanki@tops-int.com>
2025-01-28 16:29:54 +0530

    update

--- JAVASCRIPT/7.0_Array/splice_example.js
+++ JAVASCRIPT/7.0_Array/splice_example.js
@@ -1,3 +1,25 @@
-let fruits = ["Apple", "Banana", "Cherry"];
-fruits.splice(1, 1, "Orange"); // Removes 1 element at index 1 and adds "Orange"
-console.log(fruits); // Output: ["Apple", "Orange", "Cherry"]
+// let fruits = ["Apple", "Banana", "Cherry"];
+// fruits.splice(1, 1, "Orange"); // Removes 1 element at index 1 and adds "Orange"
+// console.log(fruits); // Output: ["Apple", "Orange", "Cherry"]
+
+//          0        1      2       3
+let fruits = ["banana","apple","kiwi","mango"]
+
+// console.log(fruits);
+
+// fruits.splice(1,2)
+
+console.log(fruits);
+
+// ans = fruits.splice(1,1)  // it will remove 1st index number(1) element from the existing array 
+// console.log(ans);   
+// console.log(fruits);
+
+ans = fruits.splice(0,1,"litchi")
+
+console.log(ans);
+
+console.log(fruits);
+
+    
+    
