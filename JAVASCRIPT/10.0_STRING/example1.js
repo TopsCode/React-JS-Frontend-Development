54b0003bcec64a421b93dbc90430457f5632d4dc
enji-coder <kailash.solanki@tops-int.com>
2025-01-30 14:59:16 +0530

    updates

---
+++ JAVASCRIPT/10.0_STRING/example1.js
@@ -0,0 +1,6 @@
+let string1 = 'Hello, world!';
+let string2 = "Hello, world!";
+
+console.log(string1);
+console.log(string2);
+
---
+++ JAVASCRIPT/10.0_STRING/example2.js
@@ -0,0 +1,9 @@
+let name = "John";
+let greeting = `Hello, ${name}!`;  // String interpolation
+console.log(greeting); // "Hello, John!"
+
+let multiline = `This is
+a multi-line
+string.`;
+console.log(multiline);
+
