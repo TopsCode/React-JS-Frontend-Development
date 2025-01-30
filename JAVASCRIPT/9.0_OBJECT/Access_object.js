6599170ce1fd9a1920389b32e404d6cc51e1f295
enji-coder <kailash.solanki@tops-int.com>
2025-01-30 13:14:43 +0530

    access object

---
+++ JAVASCRIPT/9.0_OBJECT/Access_object.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "pari",       // key is 'name' and value is pari
+    age: 20,            // key is 'age' and value is 30
+    isEmployed: true    // key is 'isEmployed' and value is true
+};
+
+console.log(person);
+console.log(person.name);
+
+person.age = 22;
+
+console.log(person["age"]);
+
+
