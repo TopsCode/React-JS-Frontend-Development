07a647527cb26843c561e42ad79c9154568bf68e
enji-coder <kailash.solanki@tops-int.com>
2025-01-30 13:46:59 +0530

    updates

---
+++ JAVASCRIPT/9.0_OBJECT/object_methods_example.js
@@ -0,0 +1,5 @@
+const person = { name: "pari", age: 20 };
+
+console.log(Object.keys(person));   // ["name", "age"]
+console.log(Object.values(person)); // ["pari", 20]
+console.log(Object.entries(person)); // [["name", "pari"], ["age", 20]]
