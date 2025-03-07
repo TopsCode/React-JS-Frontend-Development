933d98bc3cabf701b5b31ad2c22480d3af4f81ea
enji-coder <kailash.solanki@tops-int.com>
2025-03-07 18:10:31 +0530

    updates

---
+++ REACT_JS_PRO/myreact-app/src/6_STYLE/ButtonCom.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from "./Button.module.css";
+
+export default function ButtonCom() {
+  return (
+    <div>
+        <button className={styles.button}>Click Me</button>
+    </div>
+  )
+}
--- REACT_JS_PRO/myreact-app/src/App.jsx
+++ REACT_JS_PRO/myreact-app/src/App.jsx
@@ -5,12 +5,14 @@ import PARENTClass_Example1 from './4_PROPS/CLASS_PROPS/PARENTClass_Example1'
 import Parent_Props from './4_PROPS/DESTRUCTURE_PROPS/Parent_Props'
 import State_example from './5_STATE/State_example'
 import InlineCss1 from './6_STYLE/InlineCss1'
+import ExternalCss2 from './6_STYLE/ExternalCss2'
+import ButtonCom from './6_STYLE/ButtonCom'
 
 
 export default function App() {
   return (
     <div>
-      <InlineCss1/>
+      <ButtonCom/>
     </div>
   )
 }
