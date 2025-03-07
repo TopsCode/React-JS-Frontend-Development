8a0c00754e6308c2acc6922f905f15a0d4d88445
enji-coder <kailash.solanki@tops-int.com>
2025-03-07 12:14:25 +0530

    updates

---
+++ REACT_JS_PRO/myreact-app/src/2_FUNCTION_COMPONENTS/FunctionExample1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function FunctionExample1() {
+  return (
+    <div>
+        <h1>Hello </h1>
+        <h2>Welcome to function component</h2>
+    </div>
+  )
+}
