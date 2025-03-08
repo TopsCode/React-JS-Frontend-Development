8c8d13cc77c8b7b7b1e7f907500ef89cf2f64eae
enji-coder <kailash.solanki@tops-int.com>
2025-03-08 15:18:27 +0530

    updates

--- REACT_JS_PRO/myreact-app/src/App.jsx
+++ REACT_JS_PRO/myreact-app/src/App.jsx
@@ -4,7 +4,7 @@ import ObjectExample1 from './7_ARRAY_OBJECT_LIST/ObjectMapExample/ObjectExample
 import ObjectExample2 from './7_ARRAY_OBJECT_LIST/ObjectMapExample/ObjectExample2'
 import ArrayObjectExample from './7_ARRAY_OBJECT_LIST/ArrayObjectExample'
 import KeyExample1 from './8_KEYS_EXAMPLE/KeyExample1'
-import UseEffectExample1 from './9_API/UseEffectExample1'
+import UseEffectExample1 from './9_HOOKS/USE_EEFECT/UseEffectExample1'
 
 
 export default function App() {
