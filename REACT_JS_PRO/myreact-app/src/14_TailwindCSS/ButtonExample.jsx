a8a397648e149fb5fcd29a4d704a3a6626630882
enji-coder <kailash.solanki@tops-int.com>
2025-03-18 11:54:23 +0530

    updates

---
+++ REACT_JS_PRO/myreact-app/src/14_TailwindCSS/ButtonExample.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ButtonExample() {
+  return (
+    <div className='flex justify-center'>
+        <button className='bg-purple-700 hover:bg-blue-500 hover:text-black shadow-md text-white m-10 px-10 py-3 rounded-2xl'>
+            Click Here
+        </button>
+    </div>
+  )
+}
--- REACT_JS_PRO/myreact-app/src/App.jsx
+++ REACT_JS_PRO/myreact-app/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import Example1 from './14_TailwindCSS/Example1'
+import ButtonExample from './14_TailwindCSS/ButtonExample'
 
 
 
 export default function App() {
   return (
     <>
-      <Example1/>
+      <ButtonExample/>
     </>
   )
 }
